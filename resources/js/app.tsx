d465a6a10d77a6a5cd71053d3b47ddf1c281fee1
yanagida-troposphere <yanagida@supporter.troposphere.co.jp>
2025-08-16 12:20:32 +0900

    vite.configなど修復

---
+++ resources/js/app.tsx
@@ -0,0 +1,12 @@
+import './bootstrap';
+import '../css/app.css';
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+ReactDOM.createRoot(document.getElementById('app')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
